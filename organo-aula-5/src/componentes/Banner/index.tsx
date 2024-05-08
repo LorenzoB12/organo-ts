e5ef1ae5f4999117439fa5dcc4962537012b520c
Lorenzo <lorenzo_busolli@hotmail.com>
2024-05-07 21:21:31 -0300

    Primeiros arquivos refatorados para TypeScript

---
+++ organo-aula-5/src/componentes/Banner/index.tsx
@@ -0,0 +1,21 @@
+import './Banner.css'
+
+import React from 'react';
+
+interface BannerProps {
+    enderecoImagem: string
+    textoAlternativo?: string
+}
+
+export const Banner = ({enderecoImagem, textoAlternativo} :BannerProps) => {
+    // JSX
+    // /imagens/banner.png"
+    // O banner principal da página do Organo
+    return (
+        <header className="banner">
+            <img src={enderecoImagem} alt={textoAlternativo}/>
+        </header>
+    )
+}
+
+export default Banner;
